46139564a56cefda8aa0bbbb35b32b6c5e6bdf2b
Sebastian-Calderon <sebas.calderonmiranda@gmail.com>
2024-12-03 08:44:31 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector("#nav") 
+const menuBurguer = document.querySelector("#menu-burguer") 
+menuBurguer.addEventListener("click", ()=>{
+    navbar.classList.toggle("active")
+    console.log("Lllegue")
+})
+
+console.log("Lllegue")
+
